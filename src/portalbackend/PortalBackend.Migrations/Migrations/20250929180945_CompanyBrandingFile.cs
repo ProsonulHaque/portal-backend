0d5813c710e438d025c89c2ae0a59b770e8e9232
Prosonul Haque <proshonulhaq@gmail.com>
2025-09-30 00:12:48 +0600

    Added migrations to rename column in CompanyBrandingFile table

---
+++ src/portalbackend/PortalBackend.Migrations/Migrations/20250929180945_CompanyBrandingFile.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Org.Eclipse.TractusX.Portal.Backend.PortalBackend.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyBrandingFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "file_size_in_bytes",
+                schema: "portal",
+                table: "company_branding_files",
+                newName: "file_size_in_kilo_byte");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "file_size_in_kilo_byte",
+                schema: "portal",
+                table: "company_branding_files",
+                newName: "file_size_in_bytes");
+        }
+    }
+}
